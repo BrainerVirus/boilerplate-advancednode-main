c2ed5d730c7b80c08a03143794e8236201bc7a9c
Cristhofer Pincetti <cristhoferpincetti@hotmail.cl>
2022-08-30 16:34:35 -0400

    basic setup and first answer

---
+++ server.js
@@ -0,0 +1,22 @@
+"use strict";
+require("dotenv").config();
+const express = require("express");
+const myDB = require("./connection");
+const fccTesting = require("./freeCodeCamp/fcctesting.js");
+const path = require("node:path");
+
+const app = express();
+
+fccTesting(app); //For FCC testing purposes
+app.use("/public", express.static(process.cwd() + "/public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.route("/").get((req, res) => {
+  res.render(path.join(__dirname, "/views/pug/index.pug"));
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log("Listening on port " + PORT);
+});
