366ce640a6c80fce1fe124e7f4e501416daa77d7
Cristhofer Pincetti <cristhoferpincetti@hotmail.cl>
2022-08-31 13:16:29 -0400

    MongoDB implemented

--- connection.js
+++ connection.js
@@ -3,19 +3,45 @@ require("dotenv").config();
 const { MongoClient } = require("mongodb");
 
 async function main(callback) {
+  const URI = process.env.MONGO_URI; // Declare MONGO_URI in your .env file
+  const client = new MongoClient(URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+
   try {
-    await MongoClient.connect(process.env.MONGO_URI, (err, db) => {
-      if (err) {
-        console.log("Database error: " + err);
-      } else {
-        console.log("Connected to Database");
-      }
-    });
-  } catch (error) {
-    console.log("Database error: " + error);
-  } finally {
-    MongoClient.close();
+    // Connect to the MongoDB cluster
+    await client.connect();
+
+    // Make the appropriate DB calls
+    await callback(client);
+  } catch (e) {
+    // Catch any errors
+    console.error(e);
+    throw new Error("Unable to Connect to Database");
   }
 }
 
 module.exports = main;
+
+// // Do not change this file
+// require("dotenv").config();
+// const { MongoClient } = require("mongodb");
+
+// async function main(callback) {
+//   try {
+//     await MongoClient.connect(process.env.MONGO_URI, (err, db) => {
+//       if (err) {
+//         console.log("Database error: " + err);
+//       } else {
+//         console.log("Connected to Database");
+//       }
+//     });
+//   } catch (error) {
+//     console.log("Database error: " + error);
+//   } finally {
+//     MongoClient.close();
+//   }
+// }
+
+// module.exports = main;
