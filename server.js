a21ef88c401491f430e929c3de95ece6c98322d3
Cristhofer Pincetti <cristhoferpincetti@hotmail.cl>
2022-08-31 19:47:04 -0400

    announce new users and send and display chat messages setup

--- server.js
+++ server.js
@@ -58,13 +58,23 @@ myDB(async (client) => {
   let currentUsers = 0;
   io.on("connection", (socket) => {
     ++currentUsers;
-    io.emit("user count", currentUsers);
-    console.log("user " + socket.request.user.username + " connected");
-
+    io.emit("user", {
+      name: socket.request.user.name,
+      currentUsers,
+      connected: true,
+    });
+    socket.on("chat message", (message) => {
+      io.emit("chat message", { name: socket.request.user.name, message });
+    });
+    console.log("A user has connected");
     socket.on("disconnect", () => {
       console.log("A user has disconnected");
       --currentUsers;
-      io.emit("user count", currentUsers);
+      io.emit("user", {
+        name: socket.request.user.name,
+        currentUsers,
+        connected: false,
+      });
     });
   });
 }).catch((e) => {
